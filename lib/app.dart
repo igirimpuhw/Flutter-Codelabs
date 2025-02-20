d666626163d5a0d6897a8b3ece008d9808056050
IGIRIMPUHWE Jonathan <155901195+igirimpuhw@users.noreply.github.com>
2025-02-20 21:42:15 +0200

    Add files via upload

---
+++ lib/app.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'backdrop.dart';
+import 'login.dart';
+import 'colors.dart';
+import 'model/product.dart';
+import 'supplemental/cut_corners_border.dart';
+import 'category_menu_page.dart';
+
+// Convert ShrineApp to a StatefulWidget
+class ShrineApp extends StatefulWidget {
+  //const ShrineApp({super.key});
+
+  @override
+  _ShrineAppState createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+  // Define currentCategory variable
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext context) => const LoginPage(),
+        '/': (BuildContext context) => Backdrop(
+              currentCategory: _currentCategory,
+              frontLayer: HomePage(),
+              backLayer: CategoryMenuPage(
+                currentCategory: _currentCategory,
+                onCategoryTap: _onCategoryTap,
+              ),
+              frontTitle: const Text('SHRINE'),
+              backTitle: const Text('MENU'),
+            ),
+      },
+      theme: _kShrineTheme,
+    );
+  }
+}
+
+// Build a Shrine Theme
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePurple,
+      secondary: kShrinePurple,
+      error: kShrineErrorRed,
+    ),
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePink100,
+    ),
+    appBarTheme: const AppBarTheme(
+      foregroundColor: kShrineBrown900,
+      backgroundColor: kShrinePink100,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: CutCornersBorder(),
+      focusedBorder: CutCornersBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          color: kShrinePurple,
+        ),
+      ),
+      floatingLabelStyle: TextStyle(
+        color: kShrineBrown900,
+      ),
+    ),
+  );
+}
+
+// Build a Shrine Text Theme
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headlineSmall: base.headlineSmall!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        titleLarge: base.titleLarge!.copyWith(
+          fontSize: 18.0,
+        ),
+        bodySmall: base.bodySmall!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyLarge: base.bodyLarge!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
+}
